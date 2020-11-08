2af1ef3d5fe050757230cc05ab365a10afdf2919
sudheer <sudheer.mulinti@gmail.com>
2020-11-08 21:57:03 +0530

    adding source code and docker file- First commit

---
+++ src/test/java/com/rksvm/OpenDesk/OpenDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rksvm.OpenDesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
